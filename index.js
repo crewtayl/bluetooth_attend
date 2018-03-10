910ae043e0dd307b980d5b36b11621aed071c2ff
Crew Taylor <crewtaylor@gmail.com>
2018-03-10 13:10:14 -0800

    First Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var noble = require('noble');
+var bleno = require('bleno');
+var BlenoPrimaryService = bleno.PrimaryService;
+var EchoCharacteristic = require('./characteristic');
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+noble.on('discover', function(peripheral) {
+	
+});
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('BLE_Atenndees', ['ec00']);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+bleno.on('advertisingStart', function(err) {
+  if (!err) {
+    console.log('advertising...');
+    //
+    // Once we are advertising, it's time to set up our services,
+    // along with our characteristics.
+    //
+    bleno.setServices([
+      new BlenoPrimaryService({
+        uuid: 'ec00',
+        characteristics: [
+          new EchoCharacteristic()
+        ]
+      })
+    ]);
+  }
+});
